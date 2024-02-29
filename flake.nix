7dfea580eaa7ae9f983b63ff195b019e284c6f9c
Beisenherz, Steffen [CBC] <steffen.beisenherz@rtl.de>
2024-02-29 20:48:04 +0100

    direnv setup

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "The implementation of the interpreter/ compiler in go books";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+        perSystem = { config, self', inputs', pkgs, system, ... }:
+          let
+            name = "interpreter-go";
+            vendorHash = null;
+          in
+          {
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ self'.packages.default ];
+            };
+
+            packages = {
+              default = pkgs.buildGoModule {
+                inherit name vendorHash;
+                src = ./.;
+              };
+              docker = pkgs.dockerTools.buildImage {
+                inherit name;
+                tag = "latest";
+              };
+            };
+          };
+      };
+}
